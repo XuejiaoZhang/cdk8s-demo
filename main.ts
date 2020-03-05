84a8d50a18e053f3c5146a3ee9ddc74f66722ef4
Xuejiao Zhang <zhangxuejiaochn@hotmail.com>
2020-03-05 17:16:17 +0800

    webservice

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import { Construct } from '@aws-cdk/core';
 import { App, Chart } from 'cdk8s';
+import { WebService } from './lib/web-service';
 
 class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // define constructs here
+    new WebService(this, 'hello', { image: 'paulbouwer/hello-kubernetes:1.5', replicas: 2 });
+    new WebService(this, 'ghost', { image: 'ghost', containerPort: 2368 });
   }
 }
 
